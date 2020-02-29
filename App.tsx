574799e5999ea13527e85dc7bfeb915f1f553f12
Kaloyan Yosifov <kaloqn665@gmail.com>
2020-02-29 21:49:01 +0200

    Make safe area view colored

--- App.tsx
+++ App.tsx
@@ -17,17 +17,22 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { dark as theme, mapping } from '@eva-design/eva';
-import { ApplicationProvider } from '@ui-kitten/components';
+import { ApplicationProvider, Layout } from '@ui-kitten/components';
 
 /**
  * Internal dependencies.
  */
 import Router from '@/router';
+import { SafeAreaView } from 'react-native';
 
 const App = (): React.ReactFragment => (
     <NavigationContainer>
         <ApplicationProvider mapping={mapping} theme={theme}>
-            <Router />
+            <Layout style={{ flex: 1 }}>
+                <SafeAreaView style={{ flex: 1 }}>
+                    <Router />
+                </SafeAreaView>
+            </Layout>
         </ApplicationProvider>
     </NavigationContainer>
 );
--- src/features/auth/components/login/index.tsx
+++ src/features/auth/components/login/index.tsx
@@ -16,21 +16,17 @@ import {
 import styles from './styles';
 
 const Login = (): React.ReactFragment => (
-    <>
-        <SafeAreaView style={{ flex: 1 }}>
-            <Layout style={styles.container}>
-                <Text style={styles.text} category="h1">
-                    Welcome to UI Kitten 😻
-                </Text>
-                <Text style={styles.text} category="s1">
-                    Start with editing App.js to configure your App
-                </Text>
-                <Text style={styles.text} appearance="hint">
-                    For example, try changing theme to Dark by simply changing an import
-                </Text>
-            </Layout>
-        </SafeAreaView>
-    </>
+    <Layout style={styles.container}>
+        <Text style={styles.text} category="h1">
+            Welcome to UI Kitten 😻
+        </Text>
+        <Text style={styles.text} category="s1">
+            Start with editing App.js to configure your App
+        </Text>
+        <Text style={styles.text} appearance="hint">
+            For example, try changing theme to Dark by simply changing an import
+        </Text>
+    </Layout>
 );
 
 export default Login;

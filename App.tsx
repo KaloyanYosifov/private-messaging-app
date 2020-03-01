3ef30bab0b88d5c4e2d62aea7ae62ee7687842dc
Kaloyan Yosifov <kaloqn665@gmail.com>
2020-03-01 18:41:29 +0200

    Remove redundant comments

--- App.tsx
+++ App.tsx
@@ -1,16 +1,4 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the UI Kitten TypeScript template
- * https://github.com/akveo/react-native-ui-kitten
- *
- * Documentation: https://akveo.github.io/react-native-ui-kitten/docs
- *
- * @format
- */
-
-/**
  * External dependencies.
  */
 import React from 'react';
